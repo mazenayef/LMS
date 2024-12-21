742a5eaad2fc884cfcd15d17cd416a9a6d1d21c0
Omar_Hamed <amourahamed931@gmail.com>
2024-12-21 15:46:44 +0200

    updated endpoint url and response message

--- src/main/java/org/lms/announcement/controllers/AnnouncementController.java
+++ src/main/java/org/lms/announcement/controllers/AnnouncementController.java
@@ -4,6 +4,7 @@ package org.lms.announcement.controllers;
 import org.lms.announcement.dtos.AnnouncementDTO;
 import org.lms.announcement.models.Annoucement;
 import org.lms.announcement.services.AnnouncementService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @RestController
 @Controller
-@RequestMapping("/course/{courseId}/announcements")
+@RequestMapping("auth/course/{courseId}/announcements")
 public class AnnouncementController {
     private AnnouncementService announcementService;
 
@@ -20,9 +21,14 @@ public class AnnouncementController {
     }
 
     @PostMapping("/")
+    // only instructor can add announcement
     // announcement without attachment
     public ResponseEntity<Annoucement> addAnnouncement(@RequestBody AnnouncementDTO announcementDTO , @PathVariable("courseId") String courseId) {
-        return ResponseEntity.ok().body(announcementService.addAnnouncement(announcementDTO , courseId));
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(announcementService.addAnnouncement(announcementDTO , courseId));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 //    @PostMapping("/addwithattachment")
 //    // announcement with attachment
