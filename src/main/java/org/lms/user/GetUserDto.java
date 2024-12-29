2a6e85687fe602fb1516b78ec561efa714565dba
Mahmoud Mohammed Nael <mahmoudmnael@gmail.com>
2024-12-29 17:05:39 +0200

    Fixed bugs

--- src/main/java/org/lms/user/GetUserDto.java
+++ src/main/java/org/lms/user/GetUserDto.java
@@ -18,11 +18,46 @@ public class GetUserDto {
 		this.role = role;
 	}
 
+	public GetUserDto() {}
+
+
 	static public GetUserDto fromUser(User user) {
 		return new GetUserDto(user.getFirstName(), user.getLastName(), user.getEmail(), user.getRole());
 	}
 
 	static public List<GetUserDto> fromUsers(List<User> users) {
 		return users.stream().map(GetUserDto::fromUser).collect(Collectors.toList());
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public User.Role getRole() {
+		return role;
+	}
+
+	public void setRole(User.Role role) {
+		this.role = role;
 	}
 }
